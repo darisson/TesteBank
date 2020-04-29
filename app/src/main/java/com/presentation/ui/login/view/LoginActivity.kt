dbfedce132986760a110507831eec4ae68a430c3
darisson <darisson@softbuilder.com.br>
2020-04-28 23:45:15 -0300

    ajustes nos layouts

--- app/src/main/java/com/presentation/ui/login/view/LoginActivity.kt
+++ app/src/main/java/com/presentation/ui/login/view/LoginActivity.kt
@@ -3,30 +3,23 @@ package com.presentation.ui.login.view
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.Group
 import androidx.lifecycle.Observer
 import com.br.myapplication.R
 import com.data.source.remote.api.ApiResult
 import com.presentation.ui.home.view.HomeActivity
 import com.presentation.ui.login.viewmodel.LoginViewModel
+import kotlinx.android.synthetic.main.activity_login.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class LoginActivity : AppCompatActivity() {
 
     private val viewModel: LoginViewModel by viewModel()
 
-    private val loginButton: Button by lazy { findViewById<Button>(R.id.btnLogin) }
-    private val groupProgress: Group by lazy { findViewById<Group>(R.id.groupProgress) }
-    private val editUserLogin: EditText by lazy { findViewById<EditText>(R.id.edtUserLogin) }
-    private val editPasswordLogin: EditText by lazy { findViewById<EditText>(R.id.edtPasswordLogin) }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         setupViews()
         observerLoginResponse()
         checkHasUser()
@@ -40,7 +33,7 @@ class LoginActivity : AppCompatActivity() {
 
     private fun observerLoginResponse() {
         viewModel.liveDataResponse.observe(this, Observer {
-            groupProgress.visibility = View.GONE
+            group_progress.visibility = View.GONE
             if (it is ApiResult.Success) {
                 viewModel.saveUserPreferences(it.response.userAccount)
                 setupHomeActivity()
@@ -57,14 +50,14 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun setupViews() {
-        loginButton.setOnClickListener {
-            if (!viewModel.isValidateUserField(editUserLogin.text.toString())) {
-                editUserLogin.error = ("Usuario Invalido")
-            } else if (!viewModel.isValidPasswordField(editPasswordLogin.text.toString())) {
-                editPasswordLogin.error = ("Senha Invalida")
+        btn_login.setOnClickListener {
+            if (!viewModel.isValidateUserField(loginUser.text.toString())) {
+                loginUser.error = ("Usuario Invalido")
+            } else if (!viewModel.isValidPasswordField(passwordEdit.text.toString())) {
+                passwordEdit.error = ("Senha Invalida")
             } else {
-                groupProgress.visibility = View.VISIBLE
-                viewModel.doLogin(editUserLogin.text.toString(), editPasswordLogin.text.toString())
+                group_progress.visibility = View.VISIBLE
+                viewModel.doLogin(loginUser.text.toString(), passwordEdit.text.toString())
             }
         }
     }
