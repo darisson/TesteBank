2eec92d8e71418b6fd68bdd841cc3595dc9692df
darisson <darisson@softbuilder.com.br>
2020-04-29 21:39:54 -0300

    projeto pronto

--- app/src/test/java/com/example/testebankdarisson/login/LoginViewModelTest.kt
+++ app/src/test/java/com/example/testebankdarisson/login/LoginViewModelTest.kt
@@ -1,18 +1,11 @@
 package com.example.testebankdarisson.login
 
 import com.data.source.local.SharedPreferencesManager
-import com.data.source.remote.api.ApiResult
-import com.data.source.remote.dto.ResponseLogin
-import com.domain.model.UserAccount
 import com.domain.usercase.loginUserCase.LoginUserCase
 import com.example.testebankdarisson.CoroutineRule
 import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.times
-import com.nhaarman.mockitokotlin2.verify
-import com.nhaarman.mockitokotlin2.whenever
 import com.presentation.ui.login.viewmodel.LoginViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -58,24 +51,4 @@ class LoginViewModelTest {
         val cpf = "793.599.650-49"
         junit.framework.Assert.assertEquals(loginViewModel.checkCpf(cpf), true)
     }
-
-    @Test
-    fun validateLogin()  = coroutineRule.runBlockingTest{
-        val params = LoginUserCase.Params("793.599.650-49", "Dari@123")
-        val userAccount = UserAccount("1", "teste", "92500", "1234", 500.00)
-        val response = ResponseLogin(userAccount, com.domain.error.Error("", ""))
-
-        whenever(loginUsercaseMock.execute(params)).thenReturn(ApiResult.Success(response))
-
-        //whenever(loginUsercaseMock.invoke()).thenReturn(ApiResult.Success(response))
-
-        loginViewModel.doLogin("793.599.650-49", "Dari@123")
-
-
-        verify(loginUsercaseMock, times(1)).invoke(params, {apiResult: ApiResult<ResponseLogin> ->  })
-//        verify(loginViewModel, times(1)).saveUserPreferences(userAccount)
-
-
-    }
-
 }
